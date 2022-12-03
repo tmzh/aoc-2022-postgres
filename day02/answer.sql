3386e2dbacc249ee9410c18e46ec3b0b6a37a1df
tmzh <2162408+tmzh@users.noreply.github.com>
2022-12-03 13:46:51 +0800

    refactor day02

--- day02/answer.sql
+++ day02/answer.sql
@@ -6,35 +6,40 @@ CREATE TABLE day02.inputs
     (
         id serial,
         opp char(1),
-        player char(1)
+        strategy char(1)
     );
 
+CREATE TABLE day02.move_values (move, value)
+AS (VALUES ('X', 1),
+           ('Y', 2),
+           ('Z', 3));
 
 -- Load local file as text
-\COPY day02.inputs (opp, player) FROM 'day02/sample.txt' WITH (DELIMITER ' ');
+\COPY day02.inputs (opp, strategy) FROM 'day02/input.txt' WITH (DELIMITER ' ');
 
 /* PART ONE */
 -- A: Rock, B: Paper, C:Scissors
 -- X: Rock, Y: Paper, Z:Scissors
-  WITH shape_values (shape, value) AS (VALUES ('X', 1), ('Y', 2), ('Z', 3)),
-       outcome_values (opp, player, value) AS (VALUES ('A', 'X', 3),
+  WITH outcome_values (opp, player, value) AS (VALUES ('A', 'X', 3),
                                                       ('A', 'Y', 6),
                                                       ('B', 'Y', 3),
                                                       ('B', 'Z', 6),
                                                       ('C', 'X', 6),
                                                       ('C', 'Z', 3))
-SELECT SUM(s.value) + SUM(o.value)
+SELECT SUM(s.value) + SUM(o.value) AS part_one
   FROM day02.inputs AS i
            FULL JOIN outcome_values AS o
-           ON o.opp = i.opp AND o.player = i.player
-           FULL JOIN shape_values AS s
-           ON s.shape = i.player;
+           ON o.opp = i.opp AND o.player = i.strategy
+           FULL JOIN day02.move_values AS s
+           ON s.move = i.strategy;
 
-/* SECOND STAR */
+/* PART TWO */
 -- A:Rock, B: Paper, C:Scissors
 -- X: Lose, Y: Draw, Z: Win
-  WITH shape_values (shape, value) AS (VALUES ('A', 1), ('B', 2), ('C', 3)),
-       target_values (opp, result, player) AS (VALUES ('A', 'X', 'C'),
+
+INSERT INTO day02.move_values (VALUES ('A', 1), ('B', 2), ('C', 3));
+
+  WITH target_values (opp, result, player) AS (VALUES ('A', 'X', 'C'),
                                                       ('A', 'Y', 'A'),
                                                       ('A', 'Z', 'B'),
                                                       ('B', 'X', 'A'),
@@ -49,11 +54,11 @@ SELECT SUM(s.value) + SUM(o.value)
                                                       ('B', 'C', 6),
                                                       ('C', 'A', 6),
                                                       ('C', 'C', 3))
-SELECT SUM(s.value) + SUM(o.value)
+SELECT SUM(s.value) + SUM(o.value) AS part_two
   FROM day02.inputs AS i
            JOIN target_values AS t
-           ON i.opp = t.opp AND i.player = t.result
+           ON i.opp = t.opp AND i.strategy = t.result
            FULL JOIN outcome_values AS o
            ON o.opp = i.opp AND o.player = t.player
-           JOIN shape_values AS s
-           ON s.shape = t.player;
+           JOIN day02.move_values AS s
+           ON s.move = t.player;
